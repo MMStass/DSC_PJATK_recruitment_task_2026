e76adec8ae6d1af64c9f5517fa08171a2153e90b
Marcin Stasikowski <m.m.stass0@gmail.com>
2026-03-20 14:42:14 +0100

    Add basic FE functions in features.py

---
+++ src/features.py
@@ -0,0 +1,71 @@
+import pandas as pd
+from sklearn.preprocessing import TargetEncoder, KBinsDiscretizer
+import itertools
+
+def load_data(path_to_raw_data: str) -> tuple[pd.DataFrame, pd.DataFrame]:
+    train = pd.read_csv(f'{path_to_raw_data}/train.csv', index_col='id')
+    test = pd.read_csv(f'{path_to_raw_data}/test.csv', index_col='id')
+    return train, test
+
+def get_feature_pairs(columns: list) -> list[tuple[str, str]]:
+    return list(itertools.combinations(columns, 2))
+
+def make_categorical_interaction_features(data: pd.DataFrame, columns: list[tuple[str, str]]) -> pd.DataFrame:
+    return
+
+def make_numerical_interaction_features(data: pd.DataFrame, columns: list[tuple[str, str]]) -> pd.DataFrame:
+    return
+
+def determine_high_cardinality_features(data: pd.DataFrame, columns: list, threshold: int) -> list[str]:
+    result = []
+    for column in columns:
+        if data[column].nunique() > threshold:
+            result.append(column)
+    return result
+
+def make_count_features(data: pd.DataFrame, columns: list) -> pd.DataFrame:
+    count_features = {}
+    for column in columns:
+        name = f"{column}_count"
+        count_features[name] = data.groupby(column, dropna=False).transform('size')
+    return pd.DataFrame(count_features)
+
+def make_aggregate_features(
+        data: pd.DataFrame,
+        categorical_column: str,
+        numerical_column: str,
+        functions: list[str]
+        ) -> pd.DataFrame:
+    aggregate_features = {}
+    for function in functions:
+        name = f"{categorical_column}_{numerical_column}_{function}"
+        aggregate_features[name] = data.groupby(categorical_column, dropna=False)[numerical_column].transform(function)
+    return pd.DataFrame(aggregate_features)
+
+def make_quantile_binned_features(data: pd.DataFrame, columns: list, n_bins: int) -> pd.DataFrame:
+    quantile_binned_features = {}
+    for column in columns:
+        name = f"{column}_quantile_binned"
+        quantile_binned_features[name] = pd.qcut(data[column], q=n_bins, duplicates='drop')
+    return pd.DataFrame(quantile_binned_features)
+
+
+def make_uniform_binned_features(data: pd.DataFrame, columns: list, n_bins: int) -> pd.DataFrame:
+    quantile_binned_features = {}
+    for column in columns:
+        name = f"{column}_uniform_binned"
+        quantile_binned_features[name] = pd.cut(data[column], bins=n_bins, duplicates='drop')
+    return pd.DataFrame(quantile_binned_features)
+
+def make_kmeans_binned_features(data: pd.DataFrame, columns: list, n_bins: int) -> pd.DataFrame:
+    kmeans_binned_features = {}
+    discretizer = KBinsDiscretizer(n_bins=n_bins, encode='ordinal', strategy='kmeans')
+    for column in columns:
+        name = f"{column}_kmeans_binned"
+        kmeans_binned_features[name] = discretizer.fit_transform(data[[column]]).ravel()
+    return pd.DataFrame(kmeans_binned_features, index=data.index)
+
+
+
+
+
